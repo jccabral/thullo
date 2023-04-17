a42185bd14bc9ca66e6b506525f8a4713c8bdc86
Juan Cabral <cabraljcruz@gmail.com>
2023-04-16 22:53:18 -0300

    chore: update some configurations

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -15,13 +15,13 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'src/graphql.ts'],
+  ignorePatterns: ['.eslintrc.js', 'src/**/graphql.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    //'@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/ban-types': 'off'
   },
 };
