e3a246c6c960d3933bbdb62babe49e18977be33e
Juan Cabral <cabraljcruz@gmail.com>
2023-04-16 22:49:15 -0300

    feat: events

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { GraphQLModule } from '@nestjs/graphql'
 import { BoardModule } from './board/board.module'
 import { ListModule } from './list/list.module'
+import { EventEmitterModule } from '@nestjs/event-emitter'
 import { CardModule } from './card/card.module'
 import { GRAPHQL_MODULE_CONFIG } from './common/constants'
     BoardModule,
     ListModule,
     CardModule,
+    EventEmitterModule.forRoot({ delimiter: ':' }),
     GraphQLModule.forRoot(GRAPHQL_MODULE_CONFIG)
--- apps/api/src/common/constants.ts
+++ apps/api/src/common/constants.ts
@@ -1,4 +1,9 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+
+// Events
+export const LIST_CREATED = 'list:created'
+export const CARD_CREATED = 'card:created'
+
 // GraphQL
 export const SCHEMAS_PATH = 'src/**/*.graphql'
 export const GRAPHQL_MODULE_CONFIG: ApolloDriverConfig = {
