34eeb6880e45683790f9ac934e10e10a01ffa6a4
Juan Cabral <cabraljcruz@gmail.com>
2023-04-16 22:51:40 -0300

    feat: app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,18 @@
+import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
 import { BoardModule } from './board/board.module'
 import { ListModule } from './list/list.module'
 import { EventEmitterModule } from '@nestjs/event-emitter'
 import { CardModule } from './card/card.module'
 import { GRAPHQL_MODULE_CONFIG } from './common/constants'
+
+@Module({
+  imports: [
     BoardModule,
     ListModule,
     CardModule,
     EventEmitterModule.forRoot({ delimiter: ':' }),
     GraphQLModule.forRoot(GRAPHQL_MODULE_CONFIG)
+  ]
+})
+export class AppModule {}
